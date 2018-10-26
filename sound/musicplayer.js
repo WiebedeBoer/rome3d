5f90e6a815564c5e70388263f8f67bccc60cebab
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-27 01:50:29 +0200

    music player
    music player

---
+++ sound/musicplayer.js
@@ -0,0 +1,18 @@
+//music player
+var imperia = document.getElementById("imperia");
+function playImperium(){
+        var rand = Math.floor((Math.random() * 4) + 1);
+        if (rand ==1){
+                document.getElementById("parade").src = "music/rome_imperium.mp3";
+        }
+        else if(rand ==2){
+                document.getElementById("parade").src = "music/ancient_egyptian_cleopatra.mp3";
+        }
+        else if(rand ==3){
+                document.getElementById("parade").src = "music/parade_of_charioteers.mp3";
+        }
+        else if(rand ==4){
+                document.getElementById("parade").src = "music/celtic_king_of_the_fairies.mp3";
+        }        
+        document.getElementById("imperia").load();
+}
