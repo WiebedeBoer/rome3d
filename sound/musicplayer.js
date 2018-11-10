096a72d37e475c80ee0bbc46155b8982fb8d9fcb
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-11-10 21:56:22 +0100

    music volume
    music volume

--- sound/musicplayer.js
+++ sound/musicplayer.js
@@ -1,24 +1,60 @@
 //music player
+var aud = document.getElementById("imperia"); 
+
+//change music
 function changeImperium(){
-        var rand = Math.floor((Math.random() * 4) + 1);
-        if (rand ==1){
-                document.getElementById("parade").src = "music/rome_imperium.mp3";
-        }
+        //var rand = Math.floor((Math.random() * 7) + 1);
+
+        var rand = parseInt(document.getElementById('number').rand, 10);
+        rand = isNaN(rand) ? 2 : rand;    
+        //rand++;
+        if(rand ==1){
+                document.getElementById("parade").src = "music/Rome_5.mp3";
+                rand++;
+        } 
         else if(rand ==2){
-                document.getElementById("parade").src = "music/ancient_egyptian_cleopatra.mp3";
-        }
-        else if(rand ==3){
-                document.getElementById("parade").src = "music/parade_of_charioteers.mp3";
+                document.getElementById("parade").src = "music/Rome_4.mp3";
+                rand++;
+        } 
+        else if (rand ==3){
+                document.getElementById("parade").src = "music/rome_imperium.mp3";
+                rand++;
         }
         else if(rand ==4){
+                document.getElementById("parade").src = "music/ancient_egyptian_cleopatra.mp3";
+                rand++;
+        }
+        else if(rand ==5){
+                document.getElementById("parade").src = "music/parade_of_charioteers.mp3";
+                rand++;
+        }
+        else if(rand ==6){
                 document.getElementById("parade").src = "music/celtic_king_of_the_fairies.mp3";
-        }        
+                rand++;
+        }
+        else if(rand ==7){
+                document.getElementById("parade").src = "music/Combat_Long.mp3";
+                rand =1;
+        }  
+        document.getElementById('number').rand = rand;
         document.getElementById("imperia").load();
+        aud.play(); 
+        aud.volume = 0.1;  
 }
-var aud = document.getElementById("imperia"); 
+
+//pause music
 function pauseImperium(){        
         aud.pause(); 
 }
+
+//play music
 function playImperium(){        
         aud.play(); 
+        aud.volume = 0.1;        
+}
+
+//load music
+function loadImperium(){        
+        aud.play(); 
+        aud.volume = 0.1;        
 }
