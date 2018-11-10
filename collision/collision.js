66b67234bb8df378a9668f69bedf8c6426031f3a
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-11-10 17:45:32 +0100

    new collide
    new collide

--- collision/collision.js
+++ collision/collision.js
@@ -4,28 +4,44 @@ function Collide (){
 var originPoint = MovingCube.position.clone();
 var collisionCheck = false;
 
-clearText();
+collisionX = 0; //No collision in any direction
+collisionZ = 0; //No collision in any direction
 
 //collision
 for (var vertexIndex = 0; vertexIndex < MovingCube.geometry.vertices.length; vertexIndex++)
 {
-var localVertex = MovingCube.geometry.vertices[vertexIndex].clone();
-var globalVertex = localVertex.applyMatrix4( MovingCube.matrix );
-var directionVector = globalVertex.sub( MovingCube.position );
+    var localVertex = MovingCube.geometry.vertices[vertexIndex].clone();
+    var globalVertex = localVertex.applyMatrix4( MovingCube.matrix );
+    var directionVector = globalVertex.sub( MovingCube.position );
 
-var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
-var collisionResults = ray.intersectObjects( collidableMeshList );
-if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ){
+    var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
+    var collisionResults = ray.intersectObjects( collidableMeshList );
+    //collisions.length > 0 && collisions[0].distance <= distance
+    //if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ){
+    if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() + 0.1 ){
         collisionCheck = true;
-}
+        //If the hit is detected on ray 1, 2 or 3 the collision is in the positive X direction
+        if ((vertexIndex === 1 || vertexIndex === 2 || vertexIndex === 3)) {
+            collisionX = 1;                                
+        }
+        //If the hit is detected on ray 5, 6 or 7 the collision is in the negative X direction
+        else if ((vertexIndex === 5 || vertexIndex === 6 || vertexIndex === 7)) {
+            collisionX = -1;                                
+        }
+        //If the hit is detected on ray 0, 1 or 7 the collision is in the positive Z direction
+        if ((vertexIndex === 0 || vertexIndex === 1 || vertexIndex === 7)) {
+            collisionZ = 1;                                
+        }
+        //If the hit is detected on ray 3, 4 or 5 the collision is in the negative Z direction
+        else if ((vertexIndex === 3 || vertexIndex === 4 || vertexIndex === 5)) {
+            collisionZ = -1;                                
+        }
+    }
 
 }
 
-if (!collisionCheck){
-        if ( keyboard.pressed("d") || keyboard.pressed("s") ){
-        controls.movementSpeed = 80;}
-        else if ( keyboard.pressed("a") || keyboard.pressed("w") ){
-        controls.movementSpeed = 80;}
+    if (!collisionCheck){
+                
         //street name
         var x_street = Math.floor((camera.position.x + 1800) / 90);
         var z_street = Math.floor((camera.position.z + 1800) / 90);
@@ -42,11 +58,10 @@ if (!collisionCheck){
         //msg
         clearText();
         appendText(x_street +"th W-E street,"+z_street+"th N-S street");
-}
-else {
-        controls.movementSpeed -= controls.movementSpeed + controls.movementSpeed;  
+    }
+    else {
         clearText();     
         appendText(" boing, ");
-}
+    }
 
-}
+} 
