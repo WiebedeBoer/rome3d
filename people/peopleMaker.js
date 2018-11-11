30afc5eadc0c07dd2590d0739803aab6a3b4c547
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-11-11 21:06:20 +0100

    basilica
    basilica

--- people/peopleMaker.js
+++ people/peopleMaker.js
@@ -56,7 +56,7 @@ function PeopleMaker() {
             var people = new Person(citizen,personPositionX,personPositionZ);
             scene.add(people);            
         }
-        else if (businesses =="senatum" || businesses =="graecostacium" || businesses =="tabularium" || businesses =="atriumlibertatis"){ 
+        else if (businesses =="senatum" || businesses =="basilica" || businesses =="graecostacium" || businesses =="tabularium" || businesses =="atriumlibertatis"){ 
             var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
             var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
             citizen ="senator";         
