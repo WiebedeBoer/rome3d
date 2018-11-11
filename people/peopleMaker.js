a597ea4337ead8b7c4c4d14f8a5505fda98bef4a
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-11-11 19:55:26 +0100

    new units
    new units

--- people/peopleMaker.js
+++ people/peopleMaker.js
@@ -10,17 +10,92 @@ function PeopleMaker() {
         var jezi = (Math.floor(i / 40)) + 1;
         //column
         var itag = i - 1;
-        var businesses = xtag[itag].getElementsByTagName("BUSINESS")[0].childNodes[0].nodeValue; 
-        
-        var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
-        var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
-        
+        var businesses = xtag[itag].getElementsByTagName("BUSINESS")[0].childNodes[0].nodeValue;        
         //pleb
-        if (businesses =="bakery" || businesses =="butcher" || businesses =="tavern" || businesses =="oil" || businesses =="winery"){   
+        if (businesses =="bakery" || businesses =="butcher"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
             citizen ="pleb";         
             var people = new Person(citizen,personPositionX,personPositionZ);
-            scene.add(people);
-            console.log("pleb"+itag);
+            scene.add(people);            
+        }
+        else if (businesses =="tavern" || businesses =="oil" || businesses =="winery"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
+            citizen ="womanlow";         
+            var people = new Person(citizen,personPositionX,personPositionZ);
+            scene.add(people);            
+        }
+        //patrician
+        else if (businesses =="bank"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
+            citizen ="patrician";         
+            var people = new Person(citizen,personPositionX,personPositionZ);
+            scene.add(people);            
+        }
+        else if (businesses =="jeweler"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
+            citizen ="womanhigh";         
+            var people = new Person(citizen,personPositionX,personPositionZ);
+            scene.add(people);            
+        }
+        else if (businesses =="templum" || businesses =="regia"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
+            citizen ="pontif";         
+            var people = new Person(citizen,personPositionX,personPositionZ);
+            scene.add(people);            
+        }
+        else if (businesses =="atriumvestae"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
+            citizen ="vestalvirgin";         
+            var people = new Person(citizen,personPositionX,personPositionZ);
+            scene.add(people);            
+        }
+        else if (businesses =="senatum" || businesses =="graecostacium" || businesses =="graecostacium" || businesses =="atriumlibertatis"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
+            citizen ="senator";         
+            var people = new Person(citizen,personPositionX,personPositionZ);
+            scene.add(people);            
+        }
+        else if (businesses =="castrum"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 160; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 160; //row
+            citizen ="principes";         
+            var people = new Person(citizen,personPositionX,personPositionZ);
+            scene.add(people);            
+        }
+        else if (businesses =="amphitheatrum"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 160; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 160; //row
+            citizen ="gladiator";         
+            var people = new Person(citizen,personPositionX,personPositionZ);
+            scene.add(people);            
+        }
+        else if (businesses =="aerarium"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
+            citizen ="lictor";         
+            var people = new Person(citizen,personPositionX,personPositionZ);
+            scene.add(people);            
+        }
+        else if (businesses =="villapublica"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
+            citizen ="imperator";         
+            var people = new Person(citizen,personPositionX,personPositionZ);
+            scene.add(people);            
+        }
+        else if (businesses =="diribitorium"){ 
+            var personPositionX = (1800 - 3600) + ((40 + i - (jezi * 40)) * 90) - 70; //column
+            var personPositionZ = (1800 - 3600) + (jezi * 90) - 70; //row
+            citizen ="plebtribune";         
+            var people = new Person(citizen,personPositionX,personPositionZ);
+            scene.add(people);            
         }
 
         //increment loop
