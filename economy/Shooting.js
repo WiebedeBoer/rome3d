8a59458db6f12a9d92d3aca5de2d05637bb3f566
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-11-11 01:48:54 +0100

    shot
    shot

--- economy/Shooting.js
+++ economy/Shooting.js
@@ -1,37 +1,32 @@
 //shooting
 function Shot(){
     //shoot ammo check
-        if (totalCoin >0){
+        if (totalArrow >1){
             //less ammo
             totalArrow = totalArrow - 1;
-            totalItem = totalArrow - 1;              
-            
+            totalItem = totalArrow - 1;                        
             //create ammo particle
             var ammoGeometry = new THREE.CubeGeometry(3,3,3,1,1,1);
             var ammoMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe:true, visible:false } );
             var ammoCube = new THREE.Mesh( ammoGeometry, ammoMaterial );
-
             //set particle position
             ammoCube.position.set(camera.position.x, camera.position.y - 3, camera.position.z);
             //set rotation to that of the camera (player)
             ammoCube.rotation.x = camera.rotation.x;
             ammoCube.rotation.y = camera.rotation.y;
             ammoCube.rotation.z = camera.rotation.z;
-            
-            //create bullet mesh
-            var bulletmesh;
-
-            bulletmesh = new Arrow(camera.position.x, camera.position.y, camera.position.z,camera.rotation.x-1.5708,camera.rotation.y+1.5708,camera.rotation.z); //shop 23,20
-            //push in list
-            bulletmeshes.push(bulletmesh);
-            //add to scene
-            scene.add(bulletmesh);               
-
             //add particle
             scene.add( ammoCube );
             collidableBulletMeshList.push(ammoCube); 
-            bullets.push(ammoCube);
-
+            bullets.push(ammoCube);            
+            //create bullet mesh
+            var bulletmesh;
+            //set rotation and position to that of the camera (player)
+            bulletmesh = new Arrow(camera.position.x, camera.position.y-3, camera.position.z,camera.rotation.x,camera.rotation.y,camera.rotation.z); 
+            //push in list
+            bulletmeshes.push(bulletmesh);
+            //add to scene
+            scene.add(bulletmesh);       
             //set timeout bullet
             ammoCube.alive = true;
             setTimeout(function(){
@@ -58,12 +53,12 @@ function Shot(){
     
 } 
 //move bullet cube
-function BulletTravel(){
+function BulletTravel(delta){
     bullets.forEach(element => {
         element.translateZ(-10);
     });
     bulletmeshes.forEach(element => {
-        element.translateX(10); 
+        element.translateZ(-10); 
     });
 }
 
