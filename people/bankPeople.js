fed1571993a9ec833ce824ad7c4b01f25038e7f9
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-11-13 23:59:49 +0100

    bank light
    bank light

---
+++ people/bankPeople.js
@@ -0,0 +1,8 @@
+function BankPeople(){
+  
+    var person1 = new PersonBar("pleb",-16,16);
+    scene.add(person1);
+    var person2 = new PersonBar("patrician",17,11);
+    scene.add(person2);
+ 
+}
