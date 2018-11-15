f99dd21b7a96ee2d957d0a92de227c02c6f8d4ee
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-11-15 23:25:22 +0100

    basilica hemi
    basilica hemi

--- lighting/BasilicaLight.js
+++ lighting/BasilicaLight.js
@@ -1,6 +1,6 @@
 function basilicaLighter(LightHeight){
 //lighting
-var hemilight = new THREE.HemisphereLight( 0xfffff0, 0x101020, 0.07 );
+var hemilight = new THREE.HemisphereLight( 0xfffff0, 0x101020, 0.7 );
 hemilight.position.set( 0, LightHeight, 0 );
 scene.add( hemilight );
 
