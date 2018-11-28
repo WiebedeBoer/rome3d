614e4a064d4b884b0784406ea14a9f7b4533ee4e
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-11-28 20:57:17 +0100

    basilica people
    basilica people

---
+++ people/basilicaPeople.js
@@ -0,0 +1,14 @@
+function BasilicaPeople(){
+  
+    var person1 = new Person("pleb",-15,20);
+    scene.add(person1);
+    var person2 = new Person("patrician",15,20);
+    scene.add(person2);
+    var person3 = new Person("plebtribune",-45,105);
+    scene.add(person3);
+    var person4 = new Person("senator",15,105);
+    scene.add(person4);
+    var person5 = new Person("imperator",-15,105);
+    scene.add(person5);
+ 
+}
