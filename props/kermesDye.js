acbe0470d28c63901c8d3d9be2a8c16bc6f995e8
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-12-01 17:32:19 +0100

    dyes
    dyes

---
+++ props/kermesDye.js
@@ -0,0 +1,29 @@
+class KermesDye extends THREE.Group {
+
+    init (){
+
+        var dGun = this;
+        loadOBJModel("models/kermes/","purple_jug.obj","models/kermes/","purple_jug.mtl", (mesh) => {
+            mesh.scale.set (0.25, 0.2, 0.25);
+            dGun.position.x = this.pX;
+            dGun.position.y = this.pY;
+            dGun.position.z = this.pZ;
+            dGun.rotation.x = this.pXr;
+            dGun.rotation.y = this.pYr;
+            dGun.rotation.z = this.pZr;
+            dGun.add(mesh);
+
+        });
+    }    
+            
+    constructor(pX,pY,pZ,pXr,pYr,pZr){
+        super();
+        this.pX = pX;
+        this.pY = pY;
+        this.pZ = pZ;
+        this.pXr = pXr;
+        this.pYr = pYr;
+        this.pZr = pZr;
+        this.init();
+    }
+}
---
+++ props/tyrianDye.js
@@ -0,0 +1,29 @@
+class TyrianDye extends THREE.Group {
+
+    init (){
+
+        var dGun = this;
+        loadOBJModel("models/purple/","purple_jug.obj","models/purple/","purple_jug.mtl", (mesh) => {
+            mesh.scale.set (0.25, 0.2, 0.25);
+            dGun.position.x = this.pX;
+            dGun.position.y = this.pY;
+            dGun.position.z = this.pZ;
+            dGun.rotation.x = this.pXr;
+            dGun.rotation.y = this.pYr;
+            dGun.rotation.z = this.pZr;
+            dGun.add(mesh);
+
+        });
+    }    
+            
+    constructor(pX,pY,pZ,pXr,pYr,pZr){
+        super();
+        this.pX = pX;
+        this.pY = pY;
+        this.pZ = pZ;
+        this.pXr = pXr;
+        this.pYr = pYr;
+        this.pZr = pZr;
+        this.init();
+    }
+}
